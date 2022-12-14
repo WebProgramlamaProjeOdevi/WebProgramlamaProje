b1c8041166512ccf25cfaeae170eab7e655fe87c
salesin58 <73759052+salesin58@users.noreply.github.com>
2022-12-14 15:22:48 +0300

    entity giriş tablolar

---
+++ WebProgramlamaProje/Entity/Blogs.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebProgramlamaProje.Entity
+{
+    public class Blogs
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? imageadress { get; set; }
+        public int? CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+        public int? AuthorId { get; set; }
+        public virtual Authors Authors { get; set; }
+
+        public ICollection<Comments>  comments { get; set; }
+
+    }
+}
---
+++ WebProgramlamaProje/Entity/Citys.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebProgramlamaProje.Entity
+{
+    public class Citys
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Blogs> blogs { get; set; }
+    }
+}
