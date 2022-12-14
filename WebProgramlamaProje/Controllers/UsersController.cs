4a814287399c7722988d44cb85e61a45231328f1
mehmetalidemirtas <mehmetalidemirts@gmail.com>
2022-12-14 13:10:25 +0300

    Add project files.

---
+++ WebProgramlamaProje/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebProgramlamaProje.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult AdminLogin()
+        {
+            return View();
+        }
+
+        public IActionResult UserRegister()
+        {
+            return View();
+        }
+
+        public IActionResult UserLogin()
+        {
+            return View();
+        }
+    }
+}
