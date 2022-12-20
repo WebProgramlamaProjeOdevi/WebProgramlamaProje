d6b63fb9b3641ddf2debdd82fb34db90ddaa872e
salesin58 <73759052+salesin58@users.noreply.github.com>
2022-12-20 12:39:59 +0300

    APİ

---
+++ WebProgramlamaProje/Controllers/BlogsApiController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebProgramlamaProje.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsApiController : ControllerBase
+    {
+        private readonly BlogsApiContext _context;
+        public BlogsApiController(BlogsApiContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async<IActionResult> GET()
+        {
+
+        }
+    }
+}
