493cbc1d22fcfd744af7b66302e8b88a458bed49
salesin58 <73759052+salesin58@users.noreply.github.com>
2022-12-20 12:42:03 +0300

    APİYENİ

--- WebProgramlamaProje/Controllers/BlogsApiController.cs
+++ WebProgramlamaProje/Controllers/BlogsApiController.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebProgramlamaProje.Entity;
+using System.Linq;
+using Microsoft.CodeAnalysis.FlowAnalysis.DataFlow;
+using static System.Reflection.Metadata.BlobBuilder;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebProgramlamaProje.Controllers
 {
@@ -7,14 +12,57 @@ namespace WebProgramlamaProje.Controllers
     [ApiController]
     public class BlogsApiController : ControllerBase
     {
-        private readonly BlogsApiContext _context;
-        public BlogsApiController(BlogsApiContext context)
+        private readonly Context _context;
+        public BlogsApiController(Context context)
         {
             _context = context;
         }
         [HttpGet]
-        public async<IActionResult> GET()
+        public IEnumerable<Blog> Get()
         {
+            return _context.Blogs.Include(X=>X.Author.AuthorName).ToList();
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var blog = _context.Blogs.Find(id);
+           if (blog == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(blog);
+            }
+            
+        }
+        [HttpPost]
+        public IActionResult post([FromBody] Blog blog)
+        {
+            var _blog=new Blog();
+            _blog.AuthorID = blog.AuthorID;
+            _blog.ImageAdress = blog.ImageAdress;
+            _blog.BlogName = blog.BlogName;
+            _blog.CategoryID= blog.CategoryID;
+            _blog.Description = blog.Description;
+            if (_blog != null)
+            {
+                _context.Blogs.Add(_blog);
+                _context.SaveChanges();
+            }
+            return Ok(_blog);
+        }
+        [HttpDelete]
+        public IActionResult delete(int id)
+        { 
+            var blog = _context.Blogs.Find(id);
+            if (blog != null)
+            {
+                _context.Blogs.Remove(blog);
+                return Ok(blog);
+
+            }
+            return BadRequest();
 
         }
     }
