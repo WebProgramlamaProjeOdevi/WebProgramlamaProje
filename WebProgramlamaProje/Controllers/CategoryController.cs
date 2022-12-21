47ac321d0cb2fb6766212151b5b7a8d7c24fc38c
mehmetalidemirtas <mehmetalidemirts@gmail.com>
2022-12-21 14:21:30 +0300

    Category ekleme,silme,güncelleme

---
+++ WebProgramlamaProje/Controllers/CategoryController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebProgramlamaProje.Entity;
+using WebProgramlamaProje.Repositories;
+
+namespace WebProgramlamaProje.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult AdminCategoryList()
+        {
+            CategoryRepository categoryRepository = new CategoryRepository();
+            var categories = categoryRepository.TList();
+            return View(categories);
+        }
+
+        public IActionResult AddNewCategory(Category p)
+        {
+            CategoryRepository categoryRepository = new CategoryRepository();
+            categoryRepository.AddCategory(p);
+            return RedirectToAction("AdminCategoryList");
+        }
+        public IActionResult DeleteCategory(int id)
+        {
+            CategoryRepository categoryRepository = new CategoryRepository();
+            categoryRepository.DeleteCategory(id);
+            return RedirectToAction("AdminCategoryList");
+
+        }
+
+        [HttpGet]
+        public IActionResult EditCategory(int id)
+        {
+            CategoryRepository categoryRepository = new CategoryRepository();
+            Category category = categoryRepository.FindCategory(id);
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult EditCategory(Category p)
+        {
+            CategoryRepository categoryRepository = new CategoryRepository();
+            categoryRepository.EditCategory(p);
+            return RedirectToAction("AdminCategoryList");
+        }
+    }
+}
--- WebProgramlamaProje/Repositories/CategoryRepository.cs
+++ WebProgramlamaProje/Repositories/CategoryRepository.cs
@@ -4,5 +4,36 @@ namespace WebProgramlamaProje.Repositories
 {
 	public class CategoryRepository:GenericRepository<Category>
 	{
-	}
+
+        public int AddCategory(Category p)
+        {
+            CategoryRepository categoryRepository= new CategoryRepository();
+            if (p.CategoryName == "")
+            {
+                return -1;
+            }
+            return categoryRepository.TAdd(p);
+        }
+
+        public int DeleteCategory(int p)
+        {
+            CategoryRepository categoryRepository = new CategoryRepository();
+            Category category = categoryRepository.Find(x => x.CategoryID == p);
+            return categoryRepository.TDelete(category);
+        }
+
+        public Category FindCategory(int id)
+        {
+            CategoryRepository categoryRepository = new CategoryRepository();
+            return categoryRepository.Find(x => x.CategoryID == id);
+        }
+        public int EditCategory(Category p)
+        {
+            CategoryRepository categoryRepository = new CategoryRepository();
+
+            Category category = categoryRepository.Find(x => x.CategoryID == p.CategoryID);
+            category.CategoryName = p.CategoryName;  
+            return categoryRepository.TUpdate(category);
+        }
+    }
 }
