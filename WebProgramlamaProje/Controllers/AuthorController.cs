2c67e68781ef4f629217a779cf21912716c1555d
mehmetalidemirtas <mehmetalidemirts@gmail.com>
2022-12-21 15:21:26 +0300

    Admin için yazar listeleme, silme, güncelleme

--- WebProgramlamaProje/Controllers/AuthorController.cs
+++ WebProgramlamaProje/Controllers/AuthorController.cs
@@ -1,12 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebProgramlamaProje.Entity;
+using WebProgramlamaProje.Repositories;
 
 namespace WebProgramlamaProje.Controllers
 {
     public class AuthorController : Controller
     {
-        public IActionResult Index()
+        public IActionResult AuthorList()
         {
-            return View();
+            AuthorRepository authorRepository = new AuthorRepository();
+            var authors = authorRepository.TList();
+            return View(authors);
+        }
+        
+        public IActionResult DeleteAuthor(int id)
+        {
+            AuthorRepository authorRepository = new AuthorRepository();
+            authorRepository.DeleteAuthor(id);
+            return RedirectToAction("AuthorList");
+
+        }
+
+        [HttpGet]
+        public IActionResult EditAuthor(int id)
+        {
+            AuthorRepository authorRepository = new AuthorRepository();
+            Author author = authorRepository.FindAuthor(id);
+            return View(author);
+        }
+        [HttpPost]
+        public IActionResult EditAuthor(Author p)
+        {
+            AuthorRepository authorRepository = new AuthorRepository();
+            authorRepository.EditAuthor(p);
+            return RedirectToAction("AuthorList");
         }
     }
 }
